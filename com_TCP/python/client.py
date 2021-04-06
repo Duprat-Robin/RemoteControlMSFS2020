7ba063a1d2ec62700cd276cb3ddf4cddab3d31ba
limagolf22 <70839782+limagolf22@users.noreply.github.com>
2021-04-06 13:46:23 +0200

    Update client.py

--- com_TCP/python/client.py
+++ com_TCP/python/client.py
@@ -1,16 +1,17 @@
 import socket
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-
+s.connect(("192.168.137.1",1234))
 
 while True:
-    full_msg = ''
+    full_msg=""
     while True:
-        msg = s.recv(8)
-        if len(msg) <= 0:
-            break
-        full_msg += msg.decode("utf-8")
+        full_msg=""
+      #  msg=s.recv(100)
+       # full_msg = msg.decode("utf-8")
 
-    if len(full_msg) > 0:
-        print(full_msg)
+        #if len(full_msg) > 0:
+       #     print(full_msg)
+        time.sleep(0.1)
+        s.send(bytes("message reçu\n","utf-8"))
