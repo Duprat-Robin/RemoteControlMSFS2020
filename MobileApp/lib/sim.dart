e7efb581e60ad1d3e2820d4afa445eab30f6358b
limagolf22 <70839782+limagolf22@users.noreply.github.com>
2021-04-19 19:53:20 +0200

    Add files via upload

--- MobileApp/lib/sim.dart
+++ MobileApp/lib/sim.dart
@@ -3,11 +3,20 @@
 import 'package:flutter/material.dart';
 
 class Sim {
-  final ValueNotifier<double> speed = new ValueNotifier(0.0);
+  final ValueNotifier<double> speed = new ValueNotifier(100.0);
   final ValueNotifier<double> roll = new ValueNotifier(-30.0);
   final ValueNotifier<double> pitch = new ValueNotifier(0.0);
 
   void parseData(String message) {
+    for (String parts in message.split(" ")) {
+      if (parts.split(":")[0] == "speed") {
+        speed.value = double.parse(parts.split(":")[1]);
+      } else if (parts.split(":")[0] == "pitch") {
+        pitch.value = double.parse(parts.split(":")[1]);
+      } else if (parts.split(":")[0] == "bank") {
+        roll.value = double.parse(parts.split(":")[1]);
+      } else {}
+    }
     speed.value = double.parse(message);
   }
 }
