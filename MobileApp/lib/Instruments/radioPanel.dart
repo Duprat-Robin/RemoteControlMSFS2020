54c5aa6c95970bf751ac740060616caa7a1e8ea1
Couderc Benoit <couderc.benoit@orange.fr>
2021-06-14 11:13:50 +0200

    Radio Panel première version

--- MobileApp/lib/Instruments/radioPanel.dart
+++ MobileApp/lib/Instruments/radioPanel.dart
@@ -2,20 +2,24 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:remotecontrolmsfs/Instruments/displays.dart';
+import 'package:remotecontrolmsfs/Instruments/knobs.dart';
 import '../main.dart';
 import '../webSocket.dart';
 
-ValueNotifier<double> valFreqRadio = ValueNotifier<double>(108.500);
+//TODO les déplacer dans sim.dart ou au moins identifier la source VHF associée
+const double maxFreq = 137.000;
+const double minFreq = 108.000;
+ValueNotifier<double> valFreqRadio = ValueNotifier<double>(108.975);
 ValueNotifier<double> valFreqRadioSTBY = ValueNotifier<double>(112.200);
 
 void incrfreq(double val) {
-  valFreqRadio.value += 0.025;
+  valFreqRadioSTBY.value += 0.025;
   MyApp.webSocket
       .sendMessage(DATUM.COM_RADIO_WHOLE_INC.index.toString() + ':1');
 }
 
 void decrfreq(double val) {
-  valFreqRadio.value -= 0.025;
+  valFreqRadioSTBY.value -= 0.025;
   MyApp.webSocket
       .sendMessage(DATUM.COM_RADIO_WHOLE_DEC.index.toString() + ':1');
 }
@@ -28,22 +32,43 @@ class RadioPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Radio Panel')),
-      body: Row(
-        children: <Widget>[
-          Flexible(
-            child: FrequencyDisplay(
-              freq: valFreqRadio,
-            ),
+      backgroundColor: Colors.grey,
+      body: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Flexible(
+                child: FrequencyDisplay(
+                  freq: valFreqRadio,
+                ),
+              ),
+              Flexible(
+                child: SwitchButton(
+                  onPressed: _switchFrequencies,
+                ),
+              ),
+              Flexible(
+                child: FrequencyDisplay(
+                  freq: valFreqRadioSTBY,
+                ),
+              ),
+            ],
           ),
-          Flexible(
-            child: SwitchButton(
-              onPressed: _switchFrequencies,
-            ),
-          ),
-          Flexible(
-            child: FrequencyDisplay(
-              freq: valFreqRadioSTBY,
-            ),
+          Row(
+            children: [
+              Spacer(
+                flex: 5,
+              ),
+              Flexible(
+                child: AnalogicRotator(
+                  value: valFreqRadioSTBY,
+                  add: incrfreq,
+                  sub: decrfreq,
+                  max: maxFreq,
+                  min: minFreq,
+                ),
+              )
+            ],
           ),
         ],
       ),
@@ -75,64 +100,73 @@ class _FrequencyDisplayState extends State<FrequencyDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<double>(
-        valueListenable: widget.freq,
-        builder: (context, value, child) =>
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Flexible(
-                flex: 5,
-                child: SegmentDisplay(
-                  number: value ~/ 100,
-                ),
-              ),
-              Spacer(
-                flex: flexFactorSpacer,
-              ),
-              Flexible(
-                flex: 5,
-                child: SegmentDisplay(
-                  number: value ~/ 10 - (value ~/ 100) * 10,
-                ),
-              ),
-              Spacer(
-                flex: flexFactorSpacer,
-              ),
-              Flexible(
-                flex: 5,
-                child: SegmentDisplay(
-                  number: value.truncate() - 10 * (value ~/ 10),
-                ),
-              ),
-              Spacer(
-                flex: flexFactorSpacer,
-              ),
-              Flexible(
-                flex: 5,
-                child: SegmentDisplay(
-                  number: (value * 10).truncate() - 10 * (value.truncate()),
-                ),
-              ),
-              Spacer(
-                flex: flexFactorSpacer,
-              ),
-              Flexible(
-                flex: 5,
-                child: SegmentDisplay(
-                  number:
-                      (value * 100).truncate() - 10 * (10 * value).truncate(),
-                ),
-              ),
-              Spacer(
-                flex: flexFactorSpacer,
-              ),
-              Flexible(
-                flex: 5,
-                child: SegmentDisplay(
-                  number:
-                      (value * 1000).truncate() - 10 * (value * 100).truncate(),
-                ),
-              ),
-            ]));
+    return Container(
+      color: Colors.black,
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+      child: ValueListenableBuilder<double>(
+          valueListenable: widget.freq,
+          builder: (context, value, child) => Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      flex: 5,
+                      child: SegmentDisplay(
+                        number: value ~/ 100,
+                      ),
+                    ),
+                    Spacer(
+                      flex: flexFactorSpacer,
+                    ),
+                    Flexible(
+                      flex: 5,
+                      child: SegmentDisplay(
+                        number: value ~/ 10 - (value ~/ 100) * 10,
+                      ),
+                    ),
+                    Spacer(
+                      flex: flexFactorSpacer,
+                    ),
+                    Flexible(
+                      flex: 5,
+                      child: SegmentDisplay(
+                        number: value.truncate() - 10 * (value ~/ 10),
+                      ),
+                    ),
+                    Spacer(
+                      flex: flexFactorSpacer,
+                    ),
+                    Spacer(
+                      flex: flexFactorSpacer,
+                    ),
+                    Flexible(
+                      flex: 5,
+                      child: SegmentDisplay(
+                        number:
+                            (value * 10).truncate() - 10 * (value.truncate()),
+                      ),
+                    ),
+                    Spacer(
+                      flex: flexFactorSpacer,
+                    ),
+                    Flexible(
+                      flex: 5,
+                      child: SegmentDisplay(
+                        number: (value * 100).truncate() -
+                            10 * (10 * value).truncate(),
+                      ),
+                    ),
+                    Spacer(
+                      flex: flexFactorSpacer,
+                    ),
+                    Flexible(
+                      flex: 5,
+                      child: SegmentDisplay(
+                        number: (value * 1000).truncate() -
+                            10 * (value * 100).truncate(),
+                      ),
+                    ),
+                  ])),
+    );
   }
 
   @override
